619c284bff5576f14fa2e4a86fb85d83473369cb
iKhizarSyed <khizarsyed786@gmail.com>
2022-10-17 01:54:35 +0530

    addded time functionality

--- src/Components/navbar/NavbarComp.jsx
+++ src/Components/navbar/NavbarComp.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarComp.css"
 
 function NavbarComp() {
+    let time = new Date().toLocaleTimeString();
+    const [currentTime, setcurrentTime] = useState(time);
+
+    const updateTime = () => {
+        let time = new Date().toLocaleTimeString();
+        setcurrentTime(time);
+    }
+    setInterval(updateTime,1000);
+
+
+
+
     return (
         <div className="container-fluid">
             <nav className="navbar navbar-expand-lg">
-                <a href="/" className="navbar-brand brand-name"><i class="fa-brands fa-lg fa-kickstarter"></i>HIZAR SYED </a>
-
+                <a href="/" className="navbar-brand brand-name"><i className="fa-brands fa-lg fa-kickstarter"></i>HIZAR SYED </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto navbar-menu ">
                         <li className="nav-item ">
@@ -27,8 +39,7 @@ function NavbarComp() {
                     </ul>
                 </div>
             </nav>
-
-
+            <h5 className="time">{currentTime}</h5>
         </div>
     );
 }
